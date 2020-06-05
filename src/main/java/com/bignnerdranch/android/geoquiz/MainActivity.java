412b7e3946040323cc7fd07eb78251e677ca55d1
newchenchen <1010790262@qq.com>
2020-06-05 13:04:53 +0800

    第一次提交android项目

---
+++ src/main/java/com/bignnerdranch/android/geoquiz/MainActivity.java
@@ -0,0 +1,39 @@
+package com.bignnerdranch.android.geoquiz;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+                // Does nothing yet, but soon!
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+                // Does nothing yet, but soon!
+            }
+        });
+    }
+}
